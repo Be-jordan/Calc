968a8e14a96538a16b103d61ca3816e002dfb846
Jordan Harris <jordan@bioepic.co>
2017-02-28 13:44:01 +0000

    Return string method, Name combination method. Optional argument.

--- Calculation/Calculation/ViewControl/NameController.cs
+++ Calculation/Calculation/ViewControl/NameController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CoreGraphics;
 using UIKit;
 
 namespace Calculation
@@ -6,11 +7,31 @@ namespace Calculation
 	public class NameController : UIViewController
 	{
 
+		private string foreName;
+		private string surName;
+		private string middleName;
+
 		private UITextField _foreName;
 		private UITextField _surName;
 
+		private UIButton _combine;
+
+		private UILabel _finalName;
+
+		public string ForeName { get { return foreName; } set { foreName = value; } }
+
+		public string SurName { get { return surName; } set { surName = value; } }
+
 		public NameController()
 		{
+
+		}
+
+		public NameController(string fName, string sName, string mName = "Rae")
+		{
+			foreName = fName;
+			surName = sName;
+			middleName = mName;
 		}
 		public override void ViewDidLoad()
 		{
@@ -18,6 +39,74 @@ namespace Calculation
 
 			View.BackgroundColor = UIColor.White;
 			Title = "Combine Name";
+
+			SetForeName();
+			SetSurName();
+			SetFinalName();
+			SetNameButton();
+
+			_combine.TouchUpInside += CombineName;
+
+		}
+
+		public void SetNameButton()
+		{
+			var rect1 = new CGRect(230, 400, 75, 150);
+
+			_combine = new UIButton(rect1);
+			_combine.Layer.BorderWidth = 1;
+			_combine.Layer.CornerRadius = 5;
+			_combine.Layer.BackgroundColor = UIColor.Orange.CGColor;
+
+			_combine.SetTitle("+", UIControlState.Normal);
+			_combine.SetTitleColor(UIColor.Black, UIControlState.Normal);
+
+			View.AddSubview(_combine);
+		}
+
+		public void SetForeName()
+		{
+			var rect = new CGRect(10, 90, 300, 30);
+
+			_foreName = new UITextField(rect);
+			_foreName.BorderStyle = UITextBorderStyle.Line;
+
+			View.Add(_foreName);
+		}
+
+		public void SetSurName()
+		{
+			var rect = new CGRect(10, 130, 300, 30);
+
+			_surName = new UITextField(rect);
+			_surName.BorderStyle = UITextBorderStyle.Line;
+
+			View.Add(_surName);
+		}
+
+		private void SetFinalName()
+		{
+			var rect = new CGRect(10, 170, 300, 30);
+
+			_finalName = new UILabel(rect);
+			_finalName.TextColor = UIColor.Black;
+			_finalName.Text = "hello";
+
+			View.Add(_finalName);
+		}
+
+		public string GetName()
+		{
+			string x = foreName + " " + middleName + " " + surName;
+			return x;
+		}
+
+		public void CombineName(Object sender, EventArgs e)
+		{
+			NameController x = new NameController(_foreName.Text, _surName.Text);
+
+			_finalName.Text = string.Format("{0}", x.GetName());
+
 		}
 	}
 }
