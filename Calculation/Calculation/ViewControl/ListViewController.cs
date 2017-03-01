924952feab96e5d1a80b95946b815f7123f81e2e
Jordan Harris <jordan@bioepic.co>
2017-03-01 11:33:37 +0000

    Int list to TextBox

--- Calculation/Calculation/ViewControl/ListViewController.cs
+++ Calculation/Calculation/ViewControl/ListViewController.cs
@@ -9,8 +9,12 @@ namespace Calculation
 	{
 		private List<string> random = new List<string>();
 
+		private List<int> intList = new List<int>();
+
 		private UIButton _listButton;
-		private UITextField _listView;
+		private UIButton _listIntButton;
+		private UITextField _listStringView;
+		private UITextField _listIntView;
 
 		public ListViewController()
 		{
@@ -25,8 +29,11 @@ namespace Calculation
 
 			SetListStringView();
 			SetListStringButton();
+			SetListIntView();
+			SetListIntButton();
 
 			_listButton.TouchUpInside += AddStringContent;
+			_listIntButton.TouchUpInside += AddIntContent;
 		}
 
 		private void AddStringContent(Object sender, EventArgs e)
@@ -39,7 +46,18 @@ namespace Calculation
 			random.Add("you");
 			random.Add("?");
 
-			_listView.Text = string.Join(Environment.NewLine, random);
+			_listStringView.Text = string.Join(Environment.NewLine, random);
+		}
+
+		private void AddIntContent(Object sender, EventArgs e)
+		{
+			intList.Add(1);
+			intList.Add(2);
+			intList.Add(3);
+			intList.Add(4);
+			intList.Add(5);
+
+			_listIntView.Text = string.Join(Environment.NewLine, intList);
 		}
 
 		public void SetListStringButton()
@@ -54,17 +72,42 @@ namespace Calculation
 			View.Add(_listButton);
 		}
 
+		public void SetListIntButton()
+		{
+			var rect = new CGRect(10, 350, 300, 40);
+
+			_listIntButton = new UIButton(rect);
+			_listIntButton.SetTitle("Hello", UIControlState.Normal);
+			_listIntButton.Layer.BackgroundColor = UIColor.LightGray.CGColor;
+			_listIntButton.Layer.CornerRadius = 5;
+
+			View.Add(_listIntButton);
+		}
+
+		public void SetListIntView()
+		{
+			var rect = new CGRect(10, 130, 300, 30);
+
+			_listIntView = new UITextField(rect);
+			_listIntView.Text = "Hello";
+			_listIntView.TextColor = UIColor.Black;
+			_listIntView.BorderStyle = UITextBorderStyle.Line;
+			_listIntView.Layer.BorderColor = UIColor.Black.CGColor;
+
+			View.Add(_listIntView);
+		}
+
 		public void SetListStringView()
 		{
 			var rect = new CGRect(10, 90, 300, 30);
 
-			_listView = new UITextField(rect);
-			_listView.Text = "Hello";
-			_listView.TextColor = UIColor.Black;
-			_listView.BorderStyle = UITextBorderStyle.Line;
-			_listView.Layer.BorderColor = UIColor.Black.CGColor;
+			_listStringView = new UITextField(rect);
+			_listStringView.Text = "Hello";
+			_listStringView.TextColor = UIColor.Black;
+			_listStringView.BorderStyle = UITextBorderStyle.Line;
+			_listStringView.Layer.BorderColor = UIColor.Black.CGColor;
 
-			View.Add(_listView);
+			View.Add(_listStringView);
 		}
 
 
