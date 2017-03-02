c6372f3ad85838226ecb46dc6ad77a9fbe19d0f0
Jordan Harris <jordan@bioepic.co>
2017-03-02 14:38:21 +0000

    Random string generator

--- Calculation/Calculation/ViewControl/RandomViewController.cs
+++ Calculation/Calculation/ViewControl/RandomViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using CoreGraphics;
 using UIKit;
 
 namespace Calculation
@@ -7,6 +8,10 @@ namespace Calculation
 	public class RandomViewController : UIViewController
 	{
 		private static Random random = new Random();
+
+		private UILabel _randomLabel;
+		private UIButton _randomButton;
+
 		private static string RandomString(int length)
 		{
 			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
@@ -22,11 +27,45 @@ namespace Calculation
 			View.BackgroundColor = UIColor.White;
 			Title = "Random string generator";
 
+			SetUpTextRandom();
+			SetUpTextButton();
+
+			_randomButton.TouchUpInside += Randomise;
+
+		}
+
+		public void Randomise(Object sender, EventArgs e)
+		{
+			string ran = RandomString(12);
+
+			_randomLabel.Text = ran;
+		}
+
+		public void SetUpTextButton()
+		{
+			var rect = new CGRect(10, 400, 300, 30);
+
+			_randomButton = new UIButton(rect);
+
+			_randomButton.SetTitle("Randomise", UIControlState.Normal);
+			_randomButton.Layer.BackgroundColor = UIColor.DarkGray.CGColor;
+			_randomButton.Layer.BorderWidth = 1;
+			_randomButton.Layer.CornerRadius = 5;
+
+			View.Add(_randomButton);
 		}
 
 		public void SetUpTextRandom()
 		{
+			var rect = new CGRect(10, 300, 300, 30);
 
+			_randomLabel = new UILabel(rect);
+			_randomLabel.Text = " ";
+			_randomLabel.TextColor = UIColor.Black;
+			_randomLabel.Layer.BorderWidth = 1;
+			_randomLabel.Layer.CornerRadius = 5;
+
+			View.Add(_randomLabel);
 		}
 	}
 }
